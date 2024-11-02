740506e4ac225713ce502f884798ba6d303c30b5
Shad <shad.amezng@gmail.com>
2024-11-02 22:25:01 +0530

    added practice worksheet

---
+++ example/practice-1.js
@@ -0,0 +1,6 @@
+/**
+ * 1.1 Define variables to storing the person’s name, address and educational qualification.
+ */
+
+let name = "Kumar";
+console.log(name);
---
+++ example/practice-2.js
@@ -0,0 +1,7 @@
+/**
+ * 2.1 Define the variables for storing the person’s age, graduationStatus and yearOfGraduation.
+ */
+
+/**
+ * 2.2 Define the variables for storing the university’s yearlyFee, yearOfEstablishment, principal and isUGCApproved
+ */
